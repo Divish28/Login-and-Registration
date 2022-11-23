68a1d9d052df9f7d9b2e8a68b2c4aad2d2916d94
Divish28 <divish.karthik@aspiresys.com>
2022-11-23 09:43:33 +0530

    change

---
+++ JS/login.js
@@ -0,0 +1,26 @@
+function show(){
+    var pass=document.getElementById('password')
+    if (pass.type === 'text'){
+        pass.type='password'
+    }
+    else{
+        pass.type='text'
+    }
+}
+function storage(){
+    var user=document.getElementById('email')
+    var password=document.getElementById('password')
+    sessionStorage.setItem('email',user.value)
+    sessionStorage.setItem('pass',password.value)
+}
+function validate(){
+    let x = sessionStorage.getItem('email')
+    let y = localStorage.getItem('email')
+    if (x == y) {
+        window.location.replace("/HTML/dashbord.htlm")
+        alert("Login sucessful")
+    } else {
+        alert("invalid login")
+        window.location.href="/HTML/login.html"
+    }
+}
